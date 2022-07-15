f3dca6142f2a20d1413c8a7143fb97f3949067fd
Bagirliii <mbagirli2004@gmail.com>
2022-07-16 00:56:00 +0400

    React

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import Header from "./Components/Header";
 import Region from "./Pages/Region";
 import CountryDetails from "./Pages/CountryDetails";
 import Default from "./UI/Default";
-import { useLocation } from "react-router-dom";
 
 function App() {
-  let location = useLocation();
-  console.log(location);
   return (
     <div className="App">
       <Header />
